133d236204695b0522612a2566cf5254ac1cd598
leon <leon@mobvista.com>
2019-07-19 13:54:00 +0800

    add pricing

--- connections.go
+++ connections.go
@@ -17,6 +17,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/elb/elbiface"
 	"github.com/aws/aws-sdk-go/service/elbv2"
 	"github.com/aws/aws-sdk-go/service/elbv2/elbv2iface"
+	"github.com/aws/aws-sdk-go/service/pricing"
+	"github.com/aws/aws-sdk-go/service/pricing/pricingiface"
 )
 
 type Connections struct {
@@ -28,6 +30,7 @@ type Connections struct {
 	CostExplorer   costexploreriface.CostExplorerAPI
 	CloudFormation cloudformationiface.CloudFormationAPI
 	CloudWatch     cloudwatchiface.CloudWatchAPI
+	Pricing        pricingiface.PricingAPI
 	Region         string
 }
 
@@ -60,6 +63,7 @@ func (c *Connections) Connect(region string) {
 	costExplorerConn := make(chan *costexplorer.CostExplorer)
 	cloudFormationConn := make(chan *cloudformation.CloudFormation)
 	cloudWatchConn := make(chan *cloudwatch.CloudWatch)
+	pricingConn := make(chan *pricing.Pricing)
 
 	go func() { asConn <- autoscaling.New(c.Session) }()
 	go func() { ec2Conn <- ec2.New(c.Session) }()
@@ -68,7 +72,8 @@ func (c *Connections) Connect(region string) {
 	go func() { costExplorerConn <- costexplorer.New(c.Session) }()
 	go func() { cloudFormationConn <- cloudformation.New(c.Session) }()
 	go func() { cloudWatchConn <- cloudwatch.New(c.Session) }()
+	go func() { pricingConn <- pricing.New(c.Session) }()
 
-	c.AutoScaling, c.EC2, c.ELB, c.ELBV2, c.CostExplorer, c.CloudFormation, c.CloudWatch, c.Region =
-		<-asConn, <-ec2Conn, <-elbConn, <-elbv2Conn, <-costExplorerConn, <-cloudFormationConn, <-cloudWatchConn, region
+	c.AutoScaling, c.EC2, c.ELB, c.ELBV2, c.CostExplorer, c.CloudFormation, c.CloudWatch, c.Pricing, c.Region =
+		<-asConn, <-ec2Conn, <-elbConn, <-elbv2Conn, <-costExplorerConn, <-cloudFormationConn, <-cloudWatchConn, <-pricingConn, region
 }
